33f7f6b3baa9b0e50ca1743d55e35f6f9fb870e2
Aarnavi18 <90756308+Aarnavi18@users.noreply.github.com>
2023-04-21 17:00:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+nosex=0;
+nosey=0;
 function preload()
 {
-
+lips=loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
 }
 function setup()
 {
@@ -9,13 +11,27 @@ canvas.center();
 video=createCapture(VIDEO);
 
 video.hide();
+posenet=ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",gotposes);
 }
 function draw()
 {
 image(video,0,0,300,300);
-
+image(lips,nosex,nosey,50,20);
 }
 function take_snapshot()
 {
     save("my filter.png");
+}
+function modelLoaded()
+{
+    console.log("modelLoaded");
+}
+function gotposes(results)
+{
+if(results.length>0){
+    console.log(resuts);
+    nosex=results[0].pose.nose.x-15;
+    nosey=results[0].pose.nose.y-15;
+}
 }
